601697091f6babe1c4cd128b7987538dc8a0a559
juffy <junfeng17@gmail.com>
2014-05-11 22:47:08 +0800

    change class name to _Hd bc

--- recover url/RecoverUrl.js
+++ recover url/RecoverUrl.js
@@ -9,7 +9,7 @@ function mouseUpHander(e){
     var h3 = a.parentNode;
     if (h3.tagName != "H3" || h3.className != "r"){
         var cite = a.parentNode;
-        if (cite.tagName != "CITE" || cite.className != "_pd bc")
+        if (cite.tagName != "CITE" || cite.className != "_Hd bc")
         return;
     }
     console.log("current: " + a.href);
