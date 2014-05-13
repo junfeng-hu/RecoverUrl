4a7c090fc7214d83ff73e3b527f6480583a29248
O1.4 <junfeng17@gmail.com>
2014-05-13 15:49:45 +0800

    merge

--- recover url/RecoverUrl.js
+++ recover url/RecoverUrl.js
@@ -9,13 +9,8 @@ function mouseUpHander(e){
     var h3 = a.parentNode;
     if (h3.tagName != "H3" || h3.className != "r"){
         var cite = a.parentNode;
-<<<<<<< HEAD
         if (cite.tagName != "CITE" || (cite.className != "_pd bc" && cite.className != "_Hd bc"))
             return;
-=======
-        if (cite.tagName != "CITE" || cite.className != "_Hd bc")
-        return;
->>>>>>> e49c189dd5bb1b449a1d15678e7079053c942e3f
     }
     console.log("current: " + a.href);
     a.href = a.getAttribute("data-href");
